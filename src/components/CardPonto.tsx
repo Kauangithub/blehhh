ed3db623244a77ff3118491021aff27569312401
Kauan Machado <machadob.kauan@gmail.com>
2026-05-12 21:48:17 -0300

    Card ponto criado

--- src/components/CardPonto.tsx
+++ src/components/CardPonto.tsx
@@ -1,6 +1,23 @@
-import './CardPonto.css'
+import "./CardPonto.css"
 
+type Props = {
+    ponto: {
+        planta: string;
+        latitude: string;
+        longitude: string;
+    };
 
-export default function CardPonto() {
-   
+    trilha: string;
+};
+
+export default function CardPonto({ ponto }: Props) {
+
+    return (
+        <div className='cardTrilha'>
+            <div className="info vertical">
+            <h2>{ponto.planta}</h2>
+            </div>
+        </div>
+        
+    );
 }
--- src/pages/Explorar.tsx
+++ src/pages/Explorar.tsx
@@ -1,42 +1,91 @@
+import { useState } from 'react';
+
 import data from '../data.json';
+
 import CardTrilha from '../components/CardTrilha.tsx';
 import CardPonto from '../components/CardPonto.tsx';
-
-
-
-
+import Switch from '../components/ui/buttons/Switch.tsx';
 
 export default function Explorar() {
+
     const trilhas = [...data.trilhas];
 
+    const [trilhaSelecionada, setTrilhaSelecionada] = useState(trilhas[0]);
+
     const trilhasList = trilhas.map((trilha) => (
-        <CardTrilha key={trilha.id} trilha={trilha} />
+        <CardTrilha
+            key={trilha.id}
+            trilha={trilha}
+        />
+    ));
+
+    const pontosList = trilhaSelecionada.pontos_interesse.map((ponto, index) => (
+        <CardPonto
+            key={index}
+            ponto={ponto}
+            trilha={trilhaSelecionada.nome}
+        />
     ));
 
     return (
         <>
             <div className="paddingHeader" id='paddingImgFade'></div>
+
             <section className='img-fade' id='capivara'>
+
                 <div className="conteudo vertical">
+
                     <div className="info vertical gap5">
                         <h1>Todas as Atividades</h1>
-                        <p>Descubrar caminhos, fauna e paisagens únicas às margens do Rio Juqueriquerê</p>
+
+                        <p>
+                            Descubrar caminhos, fauna e paisagens únicas
+                            às margens do Rio Juqueriquerê
+                        </p>
                     </div>
-                    <h1>Trilhas:</h1>
+
+                    <h1>Trilhas</h1>
 
                     <div className='carrossel horizontal' id='filtros'>
                         <div className="horizontal gap5">
                             {trilhasList}
-
                         </div>
                     </div>
+
                     <div className="lista vertical">
-                        <h1>Pontos de interesse:</h1>
+
+                        <h1>Pontos de interesse</h1>
+
+                        <div className="horizontal gap5">
+
+                            {trilhas.map((trilha) => (
+
+                                <Switch
+                                    key={trilha.id}
+                                    onClick={() => setTrilhaSelecionada(trilha)}
+                                    className={
+                                        trilhaSelecionada.id === trilha.id
+                                            ? 'ativo'
+                                            : ''
+                                    }
+                                >
+                                    {trilha.nome}
+                                </Switch>
+
+                            ))}
+
+                        </div>
+
+                        <div className="vertical gap5">
+
+                            {pontosList}
+
+                        </div>
 
                     </div>
 
-                    
                 </div>
+
             </section>
         </>
     );
